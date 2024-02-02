f028687a773e7b4be9c82e2bdc00411e759b1e87
itislucyyaaa <142839187+itislucyyaaa@users.noreply.github.com>
2024-02-02 00:20:07 -0800

    Update PalindromeChecker.java

--- src/main/java/PalindromeChecker.java
+++ src/main/java/PalindromeChecker.java
@@ -37,13 +37,56 @@ public void tester()
 }
 public boolean palindrome(String word)
 {
-  //your code here
-  return false;
+  String cleanedWord = noSpaces(noCapitals(onlyLetters(word)));
+  String reversedWord = reverse(cleanedWord);
+  if(cleanedWord.equals(reversedWord))
+    return true;
+  else 
+    return false;
 }
+
 public String reverse(String str)
 {
     String sNew = new String();
-    //your code here
+    for(int i=str.length();i>0; i--){
+      sNew=sNew+str.substring(i-1,i);
+    }
     return sNew;
+}
+
+public String noCapitals(String sWord){
+  return sWord.toLowerCase();
+}
+
+public String noSpaces(String sWord){
+  String res = "";
+  for(int i=0; i<sWord.length(); i++){
+    if(sWord.substring(i,i+1).equals(" ")){
+      res=res+"";
+    }else{
+      res=res+sWord.substring(i,i+1);
+    }
+  }
+  return res;
+}
+
+public int numLetters(String sString){
+  int num=0;
+  for(int i=0; i<sString.length(); i++){
+    if(Character.isLetter(sString.charAt(i))==true){
+      num++;
+    }
+  }
+  return num;
+}
+
+public String onlyLetters(String sString){
+  String result = new String();
+  for(int i=0; i<sString.length(); i++){
+    if(Character.isLetter(sString.charAt(i))==true){
+      result = result + sString.charAt(i);
+    }
+  }
+  return result;
 }
 }
